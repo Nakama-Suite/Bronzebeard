6d7a8eb625200fd09a36453f119650ab4be940f7
The Gentleman <GentlemenSuite@proton.me>
2025-11-10 22:07:23 +0100

    Update spells.lua

--- nakama/rogue/spells.lua
+++ nakama/rogue/spells.lua
@@ -3,11 +3,10 @@
 -- Author: Gentleman
 --------------------------------------------------------------------------------
 local nakama, _A, nakama = ...
-nakama.spellBook = nakama.spellBook or {}
-nakama.spellBook.Rogue = {
+nakama.SpellBook = nakama.SpellBook or {}
+nakama.SpellBook.Rogue = {
     SinisterStrike = _A.GetSpellInfo(1101752),
     SliceAndDice   = _A.GetSpellInfo(1105171),
     Eviscerate     = _A.GetSpellInfo(1102098),
     Evasion        = _A.GetSpellInfo(1105277),
-    Throw          = _A.GetSpellInfo(2764),
 }
