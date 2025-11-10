4e4720aa52b4de0d5e1e075e0cb97f4479564821
The Gentleman <GentlemenSuite@proton.me>
2025-11-10 22:55:48 +0100

    Create colors.lua

---
+++ nakama/modules/colors.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------
+-- nakama Colors - Module
+-- Author: TheGentleman
+--------------------------------------------------------------------------------
+local nakama, _A, nakama = ...
+nakama.Colors = nakama.Colors or {}
+
+nakama.Colors.LightCyan = "|cFFE0FFFF"
+nakama.Colors.LightPink = "|cFFF48CBA"
+nakama.Colors.PeachPuff = "|cFFFFDAB9"
+nakama.Colors.Red = "|cffff0000"
+nakama.Colors.White = "|cffffffff"
