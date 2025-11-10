4e954cf11825e30d1b3058754c0a46d59f988a60
The Gentleman <GentlemenSuite@proton.me>
2025-11-10 22:07:13 +0100

    Update nakamaLoot.lua

--- nakama/modules/nakamaLoot.lua
+++ nakama/modules/nakamaLoot.lua
@@ -1,20 +1,20 @@
 --------------------------------------------------------------------------------
--- nakamaLoot - Module
+-- nakama Loot - Module
 -- Author: TheGentleman
 --------------------------------------------------------------------------------
 local nakama, _A, nakama = ...
-
+nakama.Loot = nakama.Loot or {}
 -- Local blacklist cache (O(1) lookups)
-nakama.lootBlackList = {}
+nakama.Loot.Blacklist = {}
 
 --------------------------------------------------------------------------------
 -- Listener: reset blacklist when entering combat
 --------------------------------------------------------------------------------
-function nakama.addLootListener()
+function nakama.Loot.AddListener()
     _A.Listener:Add("nakamaLoot", { "PLAYER_REGEN_DISABLED" }, function(event)
         if event == "PLAYER_REGEN_DISABLED" then
-            for k in pairs(nakama.lootBlackList) do
-                nakama.lootBlackList[k] = nil
+            for k in pairs(nakama.Loot.Blacklist) do
+                nakama.Loot.Blacklist[k] = nil
             end
         end
     end)
@@ -23,7 +23,7 @@ end
 --------------------------------------------------------------------------------
 -- Listener removal
 --------------------------------------------------------------------------------
-function nakama.deleteLootListener()
+function nakama.Loot.DeleteListener()
     _A.Listener:Remove("nakamaLoot")
 end
 
@@ -31,12 +31,11 @@ end
 -- Auto-loot routine
 -- Runs O(n) over visible corpses
 --------------------------------------------------------------------------------
-function nakama.autoLoot()
+function nakama.Loot.Auto()
     if _A.BagSpace() < 1 then return false end
 
     local corpses = _A.OM:Get("Dead")
-    local blacklist = nakama.lootBlackList
-    local now = _A.GetTime()
+    local blacklist = nakama.Loot.Blacklist
 
     for _, corpse in pairs(corpses) do
         if corpse:Hasloot() and corpse:Distance() < 4.5 then
