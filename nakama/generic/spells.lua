21168a484f90f635ac85cbca8c634dfed7fdb70d
The Gentleman <GentlemenSuite@proton.me>
2025-11-10 22:07:16 +0100

    Create spells.lua

---
+++ nakama/generic/spells.lua
@@ -0,0 +1,9 @@
+--------------------------------------------------------------------------------
+-- GentlePally - Spell Library
+-- Author: Gentleman
+--------------------------------------------------------------------------------
+local nakama, _A, nakama = ...
+nakama.SpellBook = nakama.SpellBook or {}
+nakama.SpellBook.Generic = {
+    Throw          = _A.GetSpellInfo(2764),
+}
