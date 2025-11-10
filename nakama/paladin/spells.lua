d6dedc1e4a993f87fb90004b3238078a6a8e6271
The Gentleman <GentlemenSuite@proton.me>
2025-11-10 22:07:21 +0100

    Create spells.lua

---
+++ nakama/paladin/spells.lua
@@ -0,0 +1,11 @@
+--------------------------------------------------------------------------------
+-- GentlePally - Spell Library
+-- Author: Gentleman
+--------------------------------------------------------------------------------
+local nakama, _A, nakama = ...
+nakama.SpellBook = nakama.SpellBook or {}
+nakama.SpellBook.Paladin = {
+    CrusaderStrike = _A.GetSpellInfo(1135395),
+    HolyLight = _A.GetSpellInfo(1100635),
+    SealOfRighteousness = _A.GetSpellInfo(1121084),
+}
